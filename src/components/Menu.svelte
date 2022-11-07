ef8cce57e2e74cdce0fde9f84118a4f2464fa76b
Ruletik <iam@ruletik.org>
2022-11-07 23:56:59 +0600

    Made Company Name Block + Search Input

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,14 +1,26 @@
 <script>
-    const companyLogo = new URL('/images/test_logo.jpg', import.meta.url).href
-
+    //const companyLogo = new URL("/images/test_logo.jpg", import.meta.url).href
+    const companyLogo = "/images/test_logo.jpg";
+    let companyName = "Keywords Studios";
+    import SearchInput from "../components/SearchInput.svelte"
 </script>
 
-<section class="bg-slate-100 h-screen w-56 border-slate-200 border-r-2 box-border">
 
-    <div id="companyId" class="w-full h-auto">
-        <div id="companyLogo">
-            <img src="{companyLogo}" alt="Company Name">
+<section class="bg-slate-100 h-screen w-56 border-slate-200 border-r-2 box-border ">
+
+    <div id="companyId" class="w-full h-auto py-3 px-4 flex items-center hover:bg-slate-200 active:bg-slate-300 transition cursor-pointer group">
+        
+        <div id="companyLogo" class="w-8 h-8 border-2  border-slate-200 box-border rounded-lg group-hover:shadow-md group-hover:border-slate-300 transition">
+            <img class="rounded-lg" src="{companyLogo}" alt="Company Name">
         </div>
+
+        <h6 class="font-semibold text-base ml-2 text-slate-900">{companyName}</h6>
+
+    </div>
+
+
+    <div id="searchInput" class="w-full px-4 pt-1 pb-4">
+        <SearchInput />
     </div>
 
 </section>
---
+++ src/components/SearchInput.svelte
@@ -0,0 +1,12 @@
+<form method="get">
+    <div class="relative text-gray-600 focus-within:text-gray-400">
+    <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+        <button type="submit" class="focus:outline-none focus:shadow-outline">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14 14L11.1 11.1M12.6667 7.33333C12.6667 10.2789 10.2789 12.6667 7.33333 12.6667C4.38781 12.6667 2 10.2789 2 7.33333C2 4.38781 4.38781 2 7.33333 2C10.2789 2 12.6667 4.38781 12.6667 7.33333Z" stroke="#979FAC" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>                
+        </button>
+    </span>
+    <input type="text" name="search" placeholder="Search ContentQuo" class=" transition hover:bg-slate-200 w-full h-8 text-sm text-slate-600 bg-slate-100 rounded-md pl-7 focus:outline-none focus:bg-white focus:text-slate-900 appearance-none shadow-sm border border-slate-300 leading-8" autocomplete="off" >
+    </div>
+</form>
