fac0c474f1986b61b3162e6eecfd40f2dec60892
Ruletik <iam@ruletik.org>
2022-11-07 22:24:57 +0600

    Adding Picture

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,8 +1,13 @@
+<script>
+    const companyLogo = new URL('/images/test_logo.jpg', import.meta.url).href
+
+</script>
+
 <section class="bg-slate-100 h-screen w-56 border-slate-200 border-r-2 box-border">
 
     <div id="companyId" class="w-full h-auto">
         <div id="companyLogo">
-            <img src="%sveltekit.assets%/test_logo.jpg" alt="Company Name">
+            <img src="{companyLogo}" alt="Company Name">
         </div>
     </div>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  purge: ['./src/**/*.svelte', './src/**/*.css'],
   theme: {
     extend: {}
   },
