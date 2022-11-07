6ff945aaac9d19c03a4899e98d49c869adbe77b4
Ruletik <iam@ruletik.org>
2022-11-07 22:02:26 +0600

    Learning Svelte + Tailwind

---
+++ src/components/Menu.svelte
@@ -0,0 +1,9 @@
+<section class="bg-slate-100 h-screen w-56 border-slate-200 border-r-2 box-border">
+
+    <div id="companyId" class="w-full h-auto">
+        <div id="companyLogo">
+            <img src="%sveltekit.assets%/test_logo.jpg" alt="Company Name">
+        </div>
+    </div>
+
+</section>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
   theme: {
     extend: {}
   },
